5343bd5366404332da79e30c176afc762bc024ad
José González <jgonzalez.242720@gmail.com>
2026-05-25 16:50:49 -0600

    chore(config): integrar Cloudinary para almacenamiento de archivos

--- backend/config/app.js
+++ backend/config/app.js
@@ -3,24 +3,33 @@ import cors from "cors";
 import morgan from "morgan";
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import { limiter } from "../middlewares/rate.limit.js";
 import authRoutes from "../src/auth/auth.routes.js";
+import fileRoutes from "../src/file/file.routes.js";
 
 dotenv.config();
 
 const config = (app) => {
     app.set("trust proxy", 1);
     app.use(helmet());
-    app.use(cors());
+    app.use(cors(
+        {
+            origin: 'http://localhost:5173',
+            credentials: true,
+        }
+    ));
     app.use(morgan("dev"));
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
+    app.use(cookieParser());
     app.use(limiter);
 }
 
 const routes = (app) => {
     const route = "/api/v1/miarchivo";
     app.use(`${route}/auth`, authRoutes);
+    app.use(`${route}/files`, fileRoutes);
 }
 
 export const initServer = () => {
